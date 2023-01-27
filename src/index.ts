d273ccd046159a8dbd2aeec2ad89513ba98fb090
Kellar <kellar.base09@gmail.com>
2023-01-27 15:53:05 +0000

    Add CORS, return only peerIds

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import { validateEnvVars } from "./utils/validate-env";
 import { startService } from "./start-service";
 import { logging } from "./utils/logger";
@@ -7,12 +8,14 @@ import { onlineNodes } from "./utils/dial-archaeologists";
 const app = express();
 const port = 4000;
 
+app.use(cors());
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Monitor online");
 });
 
 app.get("/online-archaeologists", (req: Request, res: Response) => {
-  res.send(onlineNodes);
+  res.send(onlineNodes.map(node => node.profile.peerId));
 });
 
 app.listen(port, async () => {
