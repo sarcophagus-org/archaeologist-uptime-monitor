2b6c08842cd39af9b90be42b797752fbd1db9b8f
Kellar <kellar.base09@gmail.com>
2023-02-06 15:52:25 +0000

    Implement dial step off

--- src/utils/dial-archaeologists.ts
+++ src/utils/dial-archaeologists.ts
@@ -4,6 +4,7 @@ import { Multiaddr, multiaddr } from "@multiformats/multiaddr";
 import { logging } from "./logger";
 import { p2pNode } from "../start-service";
 import { saveDialResults } from "./db";
+import { getPreviouslyFailedDials, updateFailedDials } from "./step-off-dials";
 
 interface Archaeologist {
   profile: any;
@@ -38,7 +39,10 @@ export async function dialArchaeologists(): Promise<Date> {
   const timestampOfDial = Date.now();
   const progressIndicator = setInterval(() => process.stdout.write("."), 5000);
 
-  const tryDial = async (arch: Archaeologist, address: Multiaddr): Promise<void> => {
+  let previouslyFialedDials = await getPreviouslyFailedDials();
+  logging.debug(`Skipping ${previouslyFialedDials.filter(p => !!p.nDialsToSkip).length} archaeologists`);
+
+  const tryDial = async (arch: Archaeologist, address: Multiaddr): Promise<boolean> => {
     try {
       await p2pNode.dial(address);
 
@@ -51,19 +55,57 @@ export async function dialArchaeologists(): Promise<Date> {
       setTimeout(async () => {
         await p2pNode.hangUp(address);
       }, 200);
+
+      return true;
     } catch (e) {
       offlineNodes.push({
         peerId: arch.profile.peerId,
         address: arch.profile.archAddress,
       });
+
+      return false;
     }
   };
 
   for (let arch of archaeologists) {
+    const previouslyFailedIndex = previouslyFialedDials.findIndex(p => p.peerId === arch.profile.peerId);
+    const hasPreviouslyFailed = previouslyFailedIndex !== -1;
+
+    if (hasPreviouslyFailed) {
+      const failedData = previouslyFialedDials[previouslyFailedIndex];
+      if (failedData.nDialsToSkip > 0) {
+        failedData.nDialsToSkip--;
+        continue;
+      }
+    }
+
     const peerIdParts = arch.profile.peerId.split(":");
     const addr = multiaddr(`/dns4/${peerIdParts[0]}/tcp/443/wss/p2p/${peerIdParts[1]}`);
 
-    await tryDial(arch, addr);
+    if (await tryDial(arch, addr)) {
+      if (hasPreviouslyFailed) {
+        // Because this previously failing node was successfully connected to on this attempt,
+        // do not skip next attempt
+        logging.debug(`Managed to reach ${arch.profile.peerId} this time`);
+        previouslyFialedDials[previouslyFailedIndex].nFails = 0;
+      }
+    } else {
+      if (hasPreviouslyFailed) {
+        // Because this previously failing node still fails,
+        // add an extra skip dial for future dials
+        const nFails = previouslyFialedDials[previouslyFailedIndex].nFails;
+        previouslyFialedDials[previouslyFailedIndex].nFails = nFails + 1;
+        previouslyFialedDials[previouslyFailedIndex].nDialsToSkip = nFails + 1;
+      } else {
+        // This is a newly failing node
+        previouslyFialedDials.push({
+          nDialsToSkip: 0,
+          nFails: 1,
+          peerId: arch.profile.peerId,
+          timestampOfDial,
+        });
+      }
+    }
   }
 
   logging.notice(`\nTotal Registered Archaeologists: ${archaeologists.length}`);
@@ -87,6 +129,8 @@ export async function dialArchaeologists(): Promise<Date> {
     onlineNodes.length,
     offlineNodes.length
   );
+
+  updateFailedDials(previouslyFialedDials.filter(p => !!p.nFails));
 
   return new Date(Date.now() + Number.parseInt(process.env.DIAL_INTERVAL_MS!));
 }
