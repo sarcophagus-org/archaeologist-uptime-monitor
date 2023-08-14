bf897a53db3f897f330151b139a7ff4588de1889
Kellar <kellar.base09@gmail.com>
2023-08-14 15:28:39 +0100

    update from main head

--- src/index.ts
+++ src/index.ts
@@ -31,45 +31,50 @@ app.get("/uptime-stats", (req: Request, res: Response) => {
     .catch(() => res.status(500));
 });
 
+app.get("/arch-uptime-statistics", (req: Request, res: Response) => {
+  getUptimeStats()
+    .then(stats => res.send(stats))
+    .catch(() => res.status(500));
+});
+
 app.get("/offline-archaeologists", (req: Request, res: Response) => {
   getOfflineNodesAddresses()
     .then(offlineList => res.send(offlineList))
     .catch(() => res.status(500));
 });
 
-
 app.get("/subgraph/arch-stats", (req: Request, res: Response) => {
-  SubgraphData.getArchStats(req.params.archAddress)
+  SubgraphData.getArchStats(req.query.archAddress as string, req.query.chainId as string)
     .then(stats => res.send(stats))
     .catch(() => res.status(500));
 });
 
 app.get("/subgraph/sarcophagus", (req: Request, res: Response) => {
-  SubgraphData.getSarcophagus(req.params.sarcoId, req.params.archAddress)
+  SubgraphData.getSarcophagus(req.query.sarcoId as string, req.query.archAddress as string, req.query.chainId as string)
     .then(sarco => res.send(sarco))
     .catch(() => res.status(500));
 });
 
 app.get("/subgraph/sarcophagi-ids", (req: Request, res: Response) => {
-  SubgraphData.getSarcophagiIds(req.params.archAddress)
+  SubgraphData.getSarcophagiIds(req.query.archAddress as string, req.query.chainId as string)
     .then(sarcoIds => res.send(sarcoIds))
     .catch(() => res.status(500));
 });
 
 app.get("/subgraph/sarcophagi", (req: Request, res: Response) => {
-  SubgraphData.getSarcophagi(req.params.archAddress)
+  SubgraphData.getSarcophagi(req.query.archAddress as string, req.query.chainId as string)
     .then(sarcos => res.send(sarcos))
     .catch(() => res.status(500));
 });
 
 app.get("/subgraph/active-sarcophagi", (req: Request, res: Response) => {
-  SubgraphData.getActiveSarcophagi(req.params.archAddress)
+  SubgraphData.getActiveSarcophagi(req.query.archAddress as string, req.query.chainId as string)
     .then(sarcos => res.send(sarcos))
     .catch(() => res.status(500));
 });
 
 app.get("/subgraph/past-sarcophagi", (req: Request, res: Response) => {
-  SubgraphData.getPastSarcophagi(req.params.archAddress)
+  SubgraphData.getPastSarcophagi(req.query.archAddress as string, req.query.chainId as string)
     .then(sarcos => res.send(sarcos))
     .catch(() => res.status(500));
 });
