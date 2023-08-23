20cf712473d13e92caca715fdc75f97e352a160f
Seth Hrbek <seth.hrbek@gmail.com>
2023-08-23 15:00:18 -0400

    remove cors to fix arch bug

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,6 @@ const whitelistedDomains = [
 
 const corsOptions = {
   origin: function (origin, callback) {
-    console.log('Received request from origin:', origin);
     if (whitelistedDomains.includes(origin) || !origin) {
       callback(null, true);
     } else {
@@ -28,73 +27,74 @@ const corsOptions = {
   }
 };
 
+app.use(cors());
 app.use(express.json());
 
-app.get("/", cors(), (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Monitor online");
 });
 
-app.get("/online-archaeologists", cors(), (req: Request, res: Response) => {
+app.get("/online-archaeologists", (req: Request, res: Response) => {
   getOnlineNodes()
     .then(onlineList => res.send(onlineList))
     .catch(() => res.status(500));
 });
 
-app.get("/uptime-stats", cors(), (req: Request, res: Response) => {
+app.get("/uptime-stats", (req: Request, res: Response) => {
   getUptimeStats()
     .then(stats => res.send(stats))
     .catch(() => res.status(500));
 });
 
-app.get("/arch-uptime-statistics", cors(), (req: Request, res: Response) => {
+app.get("/arch-uptime-statistics", (req: Request, res: Response) => {
   getUptimeStats()
     .then(stats => res.send(stats))
     .catch(() => res.status(500));
 });
 
-app.get("/offline-archaeologists", cors(), (req: Request, res: Response) => {
+app.get("/offline-archaeologists", (req: Request, res: Response) => {
   getOfflineNodesAddresses()
     .then(offlineList => res.send(offlineList))
     .catch(() => res.status(500));
 });
 
-app.get("/subgraph/arch-stats", cors(), (req: Request, res: Response) => {
+app.get("/subgraph/arch-stats", (req: Request, res: Response) => {
   SubgraphData.getArchStats(req.query.archAddress as string, req.query.chainId as string)
     .then(stats => res.send(stats))
     .catch(() => res.status(500));
 });
 
-app.get("/subgraph/sarcophagus", cors(), (req: Request, res: Response) => {
+app.get("/subgraph/sarcophagus", (req: Request, res: Response) => {
   SubgraphData.getSarcophagus(req.query.sarcoId as string, req.query.archAddress as string, req.query.chainId as string)
     .then(sarco => res.send(sarco))
     .catch(() => res.status(500));
 });
 
-app.get("/subgraph/sarcophagi-ids", cors(), (req: Request, res: Response) => {
+app.get("/subgraph/sarcophagi-ids", (req: Request, res: Response) => {
   SubgraphData.getSarcophagiIds(req.query.archAddress as string, req.query.chainId as string)
     .then(sarcoIds => res.send(sarcoIds))
     .catch(() => res.status(500));
 });
 
-app.get("/subgraph/sarcophagi", cors(), (req: Request, res: Response) => {
+app.get("/subgraph/sarcophagi", (req: Request, res: Response) => {
   SubgraphData.getSarcophagi(req.query.archAddress as string, req.query.chainId as string)
     .then(sarcos => res.send(sarcos))
     .catch(() => res.status(500));
 });
 
-app.get("/subgraph/active-sarcophagi", cors(), (req: Request, res: Response) => {
+app.get("/subgraph/active-sarcophagi", (req: Request, res: Response) => {
   SubgraphData.getActiveSarcophagi(req.query.archAddress as string, req.query.chainId as string)
     .then(sarcos => res.send(sarcos))
     .catch(() => res.status(500));
 });
 
-app.get("/subgraph/past-sarcophagi", cors(), (req: Request, res: Response) => {
+app.get("/subgraph/past-sarcophagi", (req: Request, res: Response) => {
   SubgraphData.getPastSarcophagi(req.query.archAddress as string, req.query.chainId as string)
     .then(sarcos => res.send(sarcos))
     .catch(() => res.status(500));
 });
 
-app.get("/bundlr/publicKey", cors(corsOptions), async (req: Request, res: Response) => {
+app.get("/bundlr/publicKey", async (req: Request, res: Response) => {
   const key = process.env.BUNDLR_PAYMENT_PRIVATE_KEY!;
   if (!key) throw new Error("Private key is undefined!");
 
@@ -102,7 +102,7 @@ app.get("/bundlr/publicKey", cors(corsOptions), async (req: Request, res: Respon
   res.status(200).json({ publicKey: signer.publicKey.toString("hex") });
 });
 
-app.post("/bundlr/signData", cors(corsOptions), async (req: Request, res: Response) => {
+app.post("/bundlr/signData", async (req: Request, res: Response) => {
   const { messageData } = req.body;
 
   if (!messageData) {
