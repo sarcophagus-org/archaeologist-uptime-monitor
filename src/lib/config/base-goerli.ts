e032b49ec7be59edcf295ac53fcee027338fbb40
Kellar <kellar.base09@gmail.com>
2023-09-14 16:36:17 +0100

    Add base, poly

---
+++ src/lib/config/base-goerli.ts
@@ -0,0 +1,9 @@
+import { NetworkConfig } from "../types/network-config";
+
+export const baseGoerliConfig: NetworkConfig = {
+  chainId: 84531,
+  networkName: "Base Goerli Testnet",
+  networkShortName: "baseGoerli",
+  sarcoTokenAddress: "0x2BC9019e6d9e6a26D7D8d8CDDa4e5dE9B787D7bb",
+  diamondDeployAddress: "0xB933926f50b33797d0fa1DaEe65D5830224E53E1",
+};
---
+++ src/lib/config/polygon-mumbai.ts
@@ -0,0 +1,9 @@
+import { NetworkConfig } from "../types/network-config";
+
+export const polygonMumbaiNetworkConfig: NetworkConfig = {
+  chainId: 80001,
+  networkName: "Polygon Mumbai Testnet",
+  networkShortName: "PolygonMumbai",
+  sarcoTokenAddress: "0x2BC9019e6d9e6a26D7D8d8CDDa4e5dE9B787D7bb",
+  diamondDeployAddress: "0x42F2C41e0285B3CBED8084b2c7476F11730935Bc",
+};
